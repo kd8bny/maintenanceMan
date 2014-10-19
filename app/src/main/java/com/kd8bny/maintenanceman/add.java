6f7d1ca6612e42ae553772194ec2b2d2463e543c
kd8bny <kd8bny@gmail.com>
2014-10-18 22:21:18 -0400

    Added add vehicle

--- app/src/main/java/com/kd8bny/maintenanceman/add.java
+++ app/src/main/java/com/kd8bny/maintenanceman/add.java
@@ -1,40 +1,28 @@
 package com.kd8bny.maintenanceman;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-
-public class overview extends Activity {
-
-    private Button maddButton;
+/**
+ * Created by kd8bny on 10/18/14.
+ */
+public class add extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_overview);
-
-        maddButton = (Button)findViewById(R.id.button_add);
-        maddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(overview.this, add.class);
-                startActivity(i);
-            }
-        });
+        setContentView(R.layout.activity_add);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.overview, menu);
-        return true;
-    }
+    //    getMenuInflater().inflate(R.menu.add, menu);
+    //    return true;
+    //}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
