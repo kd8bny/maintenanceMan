c0e429451c9365f9254e67a2744ea27cc22779aa
kd8bny <kd8bny@gmail.com>
2014-10-20 16:19:00 -0400

    Fixed null pointer. Started fragments

--- app/src/main/java/com/kd8bny/maintenanceman/overview.java
+++ app/src/main/java/com/kd8bny/maintenanceman/overview.java
@@ -1,8 +1,11 @@
 package com.kd8bny.maintenanceman;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,21 +14,34 @@ import android.widget.Button;
 
 public class overview extends Activity {
 
-    private Button maddButton;
+    private static final String TAG = "overviewActivity";
+
+    //private Button maddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
 
-        maddButton = (Button)findViewById(R.id.button_add);
-        maddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(overview.this, add.class);
-                startActivity(i);
-            }
-        });
+        Log.d(TAG, "testing 123");
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = new addVehicle();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+        //maddButton = (Button)findViewById(R.id.button_add);
+        //maddButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Intent i = new Intent(overview.this, add.class);
+        //        startActivity(i);
+        //    }
+        //});
     }
 
 
