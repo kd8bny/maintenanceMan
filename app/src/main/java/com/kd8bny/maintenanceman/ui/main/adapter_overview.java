2d83dcb1ea3afbce6581c68aca838becb32d2f34
kd8bny <kd8bny@gmail.com>
2015-01-09 20:52:07 -0500

    Items clickable!

--- app/src/main/java/com/kd8bny/maintenanceman/ui/main/adapter_overview.java
+++ app/src/main/java/com/kd8bny/maintenanceman/ui/main/adapter_overview.java
@@ -1,19 +1,24 @@
 package com.kd8bny.maintenanceman.ui.main;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kd8bny.maintenanceman.R;
+import com.kd8bny.maintenanceman.ui.add.activity_add_fleetRoster;
+import com.kd8bny.maintenanceman.ui.add.activity_vehicleEvent;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class adapter_overview extends RecyclerView.Adapter<adapter_overview.AdapterViewHolder> {
+public class adapter_overview extends RecyclerView.Adapter<adapter_overview.AdapterViewHolder>{
     private static final String TAG = "adapter_overview";
 
     public ArrayList<ArrayList> vehicleList = new ArrayList<>();
@@ -41,26 +46,43 @@ public class adapter_overview extends RecyclerView.Adapter<adapter_overview.Adap
             adapterViewHolder.vyear.setText(vehicleSpecs.get(1));
         }
     }
+
     @Override
     public AdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View itemView = LayoutInflater.
                 from(viewGroup.getContext()).
                 inflate(R.layout.overview_card, viewGroup, false);
-        return new AdapterViewHolder(itemView);
+        return new AdapterViewHolder(itemView, vehicleList);
     }
-    public static class AdapterViewHolder extends RecyclerView.ViewHolder {
+
+    public static class AdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         protected TextView vyear;
         protected TextView vmake;
         protected TextView vmodel;
         protected TextView vengine;
-        public AdapterViewHolder(View v) {
-            super(v);
-            vyear = (TextView) v.findViewById(R.id.year);
-            vmake = (TextView) v.findViewById(R.id.make);
-            vmodel = (TextView) v.findViewById(R.id.model);
-            vengine = (TextView) v.findViewById(R.id.engine);
+
+        public AdapterViewHolder(View view, final ArrayList<ArrayList> vehicleList) {
+            super(view);
+
+            view.setTag(vehicleList);
+            view.setOnClickListener(this);
+
+            vyear = (TextView) view.findViewById(R.id.year);
+            vmake = (TextView) view.findViewById(R.id.make);
+            vmodel = (TextView) view.findViewById(R.id.model);
+            vengine = (TextView) view.findViewById(R.id.engine);
         }
-    }
+
+        @Override
+        public void onClick(View view) {
+            ArrayList<ArrayList> vehicleList = (ArrayList<ArrayList>) view.getTag();
+
+            Intent viewIntent = new Intent(view.getContext(), activity_vehicleEvent.class);
+            viewIntent.putStringArrayListExtra("vehicleSent", vehicleList.get(getPosition()));
+            view.getContext().startActivity(viewIntent);
+        }
+
+}
 }
 
 
