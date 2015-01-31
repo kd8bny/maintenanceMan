8481e82a2a0b8b1409291b323354b72dffc42d18
kd8bny <kd8bny@gmail.com>
2015-01-31 16:03:45 -0500

    AFTER LAYOUT HELL... Dynamic cards are complete!!!!

--- app/src/main/java/com/kd8bny/maintenanceman/ui/history/adapter_info.java
+++ app/src/main/java/com/kd8bny/maintenanceman/ui/history/adapter_info.java
@@ -1,22 +1,33 @@
 package com.kd8bny.maintenanceman.ui.history;
 
-import android.content.Intent;
+import android.content.res.TypedArray;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.kd8bny.maintenanceman.R;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class adapter_info extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     private static final String TAG = "adapter_info";
 
     public ArrayList<ArrayList> vehicleInfo = new ArrayList<>();
     public ArrayList<String> cardInfo = new ArrayList<>();
+
+    protected View itemViewGeneral;
+    protected View itemViewTires;
+    protected View itemViewEngine;
 
     private static final int VIEW_GENERAL = 0;
     private static final int VIEW_ENGINE = 1;
@@ -40,21 +51,24 @@ public class adapter_info extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         switch(viewType){
             case VIEW_ENGINE:
-                View itemViewEngine = LayoutInflater.
-                        from(viewGroup.getContext()).
-                        inflate(R.layout.info_card_eng, viewGroup, false);
+                itemViewEngine = LayoutInflater
+                        .from(viewGroup.getContext())
+                        .inflate(R.layout.card_info, viewGroup, false);
+                cardInfo.clear();
                 return new ViewHolderEngine(itemViewEngine, cardInfo);
 
             case VIEW_TIRES:
-                View itemViewTires = LayoutInflater.
-                        from(viewGroup.getContext()).
-                        inflate(R.layout.info_card_tires, viewGroup, false);
+                itemViewTires = LayoutInflater
+                        .from(viewGroup.getContext())
+                        .inflate(R.layout.card_info, viewGroup, false);
+                cardInfo.clear();
                 return new ViewHolderTires(itemViewTires, cardInfo);
 
             default: //General
-                View itemViewGeneral = LayoutInflater.
-                        from(viewGroup.getContext()).
-                        inflate(R.layout.info_card_gen, viewGroup, false);
+                itemViewGeneral = LayoutInflater
+                        .from(viewGroup.getContext())
+                        .inflate(R.layout.card_info, viewGroup, false);
+                cardInfo.clear();
                 return new ViewHolderGeneral(itemViewGeneral, cardInfo);
         }
 
@@ -65,26 +79,17 @@ public class adapter_info extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         cardInfo = vehicleInfo.get(i);
         switch (getItemViewType(i)) {
             case VIEW_GENERAL:
-                ViewHolderGeneral viewHolderGeneral = (ViewHolderGeneral) viewHolder;
-                viewHolderGeneral.vyear.setText(cardInfo.get(0));
-                viewHolderGeneral.vmake.setText(cardInfo.get(1));
-                viewHolderGeneral.vmodel.setText(cardInfo.get(2));
-                viewHolderGeneral.vplate.setText(cardInfo.get(3)); //TODO dict
+                new ViewHolderGeneral(itemViewGeneral, cardInfo);
 
                 break;
 
             case VIEW_ENGINE:
-                ViewHolderEngine viewHolderEngine = (ViewHolderEngine) viewHolder;
-                viewHolderEngine.vsize.setText(cardInfo.get(0));
-                viewHolderEngine.vfilter.setText(cardInfo.get(1));
-                viewHolderEngine.vweight.setText(cardInfo.get(2));//TODO dict
+                new ViewHolderEngine(itemViewEngine, cardInfo);
 
                 break;
 
             case VIEW_TIRES:
-                ViewHolderTires viewHolderTires = (ViewHolderTires) viewHolder;
-                viewHolderTires.vsummerSize.setText(cardInfo.get(0));
-                viewHolderTires.vwinterSize.setText(cardInfo.get(1));//TODO dict
+                new ViewHolderTires(itemViewTires, cardInfo);
 
                 break;
             }
@@ -92,53 +97,238 @@ public class adapter_info extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     }
 
     class ViewHolderGeneral extends RecyclerView.ViewHolder {
-        protected TextView vyear;
-        protected TextView vmake;
-        protected TextView vmodel;
-        protected TextView vplate;
+        private static final String TAG = "adapter_info_ViewHolder_gen";
 
-        public ViewHolderGeneral(View view, final ArrayList<String> vehicleList) {
+        private TypedArray headerColors;
+        private ArrayList<String> labels = new ArrayList<>();
+
+        public ViewHolderGeneral(View view, final ArrayList<String> cardInfo) {
             super(view);
 
-            //view.setTag(vehicleList);
+            View layout = view.findViewById(R.id.card_info_lin);
 
-            //TODO card views these are null
-            vyear = (TextView) view.findViewById(R.id.year);
-            vmake = (TextView) view.findViewById(R.id.make);
-            vmodel = (TextView) view.findViewById(R.id.model);
-            vplate = (TextView) view.findViewById(R.id.plate);
+            DisplayMetrics metrics = layout.getContext().getResources().getDisplayMetrics();
+            float hMdp =  view.getResources().getDimension(R.dimen.start_end_horizontal_margin);
+            float vMdp =  view.getResources().getDimension(R.dimen.heading_sub_vertical_margin);
+            float vCSdp = view.getResources().getDimension(R.dimen.content_vertical_space);
+            float hCSdp = view.getResources().getDimension(R.dimen.content_horizontal_margin);
+
+            int hMargin = (int) (hMdp/metrics.density + 0.5f);
+            int vMargin = (int) (vMdp/metrics.density + 0.5f);
+            int vcontentSpace = (int) (vCSdp/metrics.density + 0.5f);
+            int hcontentSpace = (int) (hCSdp/metrics.density + 0.5f);
+
+            float fieldsp = view.getResources().getDimension(R.dimen.field_font);
+            float headersp = view.getResources().getDimension(R.dimen.header_font);
+            int fieldSize = (int) (fieldsp/metrics.density + 0.5f);
+            int headerSize = (int) (headersp/metrics.density + 0.5f);
+
+            Log.i(TAG,hMdp+"");
+            Log.i(TAG,metrics.density+"den");
+            Log.i(TAG,hMargin+"");
+
+            HashSet tempHS = new HashSet();
+            tempHS.addAll(cardInfo);
+
+            if(tempHS.size()>1) {
+                headerColors = view.getResources().obtainTypedArray(R.array.header_color);
+
+                labels.add(view.getResources().getString(R.string.spec_year));
+                labels.add(view.getResources().getString(R.string.spec_make));
+                labels.add(view.getResources().getString(R.string.spec_model));
+                labels.add(view.getResources().getString(R.string.spec_plate));
+
+
+
+                //Header
+                TextView tempHeaderTitle = new TextView(view.getContext());
+
+                tempHeaderTitle.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                tempHeaderTitle.setPadding(hMargin, vMargin, hMargin, hMargin);
+                tempHeaderTitle.setTextColor(view.getResources().getColor(R.color.header));
+                tempHeaderTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, headerSize);
+                tempHeaderTitle.setText(R.string.header_general);
+                tempHeaderTitle.setBackgroundColor(headerColors.getColor(0, 0));
+
+                ((LinearLayout) layout).addView(tempHeaderTitle);
+
+                for (int i = 0; i < cardInfo.size(); i++) {
+                    if (!cardInfo.get(i).isEmpty()) {
+                        TextView tempTextView = new TextView(view.getContext());
+
+                        tempTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                        tempTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fieldSize);
+                        tempTextView.setTextColor(view.getResources().getColor(R.color.secondary_text));
+
+                        tempTextView.setPadding(hcontentSpace, vcontentSpace, hcontentSpace, vcontentSpace);
+                        tempTextView.setGravity(Gravity.START);
+
+
+                        tempTextView.setText(labels.get(i) + ":     " + cardInfo.get(i));
+
+                        ((LinearLayout) layout).addView(tempTextView);
+
+                    }
+                }
+            }
         }
     }
 
     class ViewHolderEngine extends RecyclerView.ViewHolder {
-        protected TextView vsize;
-        protected TextView vfilter;
-        protected TextView vweight;
+        private static final String TAG = "adapter_info_ViewHolder_eng";
 
-        public ViewHolderEngine(View view, final ArrayList<String> vehicleList) {
+        private TypedArray headerColors;
+        private ArrayList<String> labels = new ArrayList<>();
+
+        public ViewHolderEngine(View view, final ArrayList<String> cardInfo) {
             super(view);
 
-            //view.setTag(vehicleList);
+            View layout = view.findViewById(R.id.card_info_lin);
 
-            //TODO card views these are null
-            vsize = (TextView) view.findViewById(R.id.eng);
-            vfilter = (TextView) view.findViewById(R.id.val_spec_oil_filter);
-            vweight = (TextView) view.findViewById(R.id.val_spec_oil_weight);
+            DisplayMetrics metrics = layout.getContext().getResources().getDisplayMetrics();
+            float hMdp =  view.getResources().getDimension(R.dimen.start_end_horizontal_margin);
+            float vMdp =  view.getResources().getDimension(R.dimen.heading_sub_vertical_margin);
+            float vCSdp = view.getResources().getDimension(R.dimen.content_vertical_space);
+            float hCSdp = view.getResources().getDimension(R.dimen.content_horizontal_margin);
+
+            int hMargin = (int) (hMdp/metrics.density + 0.5f);
+            int vMargin = (int) (vMdp/metrics.density + 0.5f);
+            int vcontentSpace = (int) (vCSdp/metrics.density + 0.5f);
+            int hcontentSpace = (int) (hCSdp/metrics.density + 0.5f);
+
+            float fieldsp = view.getResources().getDimension(R.dimen.field_font);
+            float headersp = view.getResources().getDimension(R.dimen.header_font);
+            int fieldSize = (int) (fieldsp/metrics.density + 0.5f);
+            int headerSize = (int) (headersp/metrics.density + 0.5f);
+
+            HashSet tempHS = new HashSet();
+            tempHS.addAll(cardInfo);
+
+            if(tempHS.size()>1) {
+                headerColors = view.getResources().obtainTypedArray(R.array.header_color);
+
+                labels.add(view.getResources().getString(R.string.spec_engine));
+                labels.add(view.getResources().getString(R.string.spec_oil_filter));
+                labels.add(view.getResources().getString(R.string.spec_oil_weight));
+
+                //Header
+                TextView tempHeaderTitle = new TextView(view.getContext());
+
+                tempHeaderTitle.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                tempHeaderTitle.setPadding(hMargin, vMargin, hMargin, hMargin);
+                tempHeaderTitle.setTextColor(view.getResources().getColor(R.color.header));
+                tempHeaderTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, headerSize);
+                tempHeaderTitle.setText(R.string.header_engine);
+                tempHeaderTitle.setBackgroundColor(headerColors.getColor(1, 0));
+
+                ((LinearLayout) layout).addView(tempHeaderTitle);
+
+                for (int i = 0; i < cardInfo.size(); i++) {
+                    if (!cardInfo.get(i).isEmpty()) {
+                        TextView tempTextView = new TextView(view.getContext());
+
+                        tempTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                        tempTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fieldSize);
+                        tempTextView.setTextColor(view.getResources().getColor(R.color.secondary_text));
+
+                        tempTextView.setPadding(hcontentSpace, vcontentSpace, hcontentSpace, vcontentSpace);
+                        tempTextView.setGravity(Gravity.START);
+
+                        tempTextView.setText(labels.get(i) + ":     " + cardInfo.get(i));
+
+                        ((LinearLayout) layout).addView(tempTextView);
+
+                    }
+                }
+            }
         }
     }
 
-        class ViewHolderTires extends RecyclerView.ViewHolder {
-            protected TextView vsummerSize;
-            protected TextView vwinterSize;
 
-            public ViewHolderTires(View view, final ArrayList<String> vehicleList) {
-                super(view);
+    class ViewHolderTires extends RecyclerView.ViewHolder {
+        private static final String TAG = "adapter_info_ViewHolder_tires";
 
-                //view.setTag(vehicleList);
+        private TypedArray headerColors;
+        private ArrayList<String> labels = new ArrayList<>();
 
-                //TODO card views these are null
-                vsummerSize = (TextView) view.findViewById(R.id.summer_size);
-                vwinterSize = (TextView) view.findViewById(R.id.winter_size);
+
+        public ViewHolderTires(View view, final ArrayList<String> cardInfo) {
+            super(view);
+
+            View layout = view.findViewById(R.id.card_info_lin);
+
+            DisplayMetrics metrics = layout.getContext().getResources().getDisplayMetrics();
+            float hMdp =  view.getResources().getDimension(R.dimen.start_end_horizontal_margin);
+            float vMdp =  view.getResources().getDimension(R.dimen.heading_sub_vertical_margin);
+            float vCSdp = view.getResources().getDimension(R.dimen.content_vertical_space);
+            float hCSdp = view.getResources().getDimension(R.dimen.content_horizontal_margin);
+
+            int hMargin = (int) (hMdp/metrics.density + 0.5f);
+            int vMargin = (int) (vMdp/metrics.density + 0.5f);
+            int vcontentSpace = (int) (vCSdp/metrics.density + 0.5f);
+            int hcontentSpace = (int) (hCSdp/metrics.density + 0.5f);
+
+            float fieldsp = view.getResources().getDimension(R.dimen.field_font);
+            float headersp = view.getResources().getDimension(R.dimen.header_font);
+            int fieldSize = (int) (fieldsp/metrics.density + 0.5f);
+            int headerSize = (int) (headersp/metrics.density + 0.5f);
+
+            HashSet tempHS = new HashSet();
+            tempHS.addAll(cardInfo);
+
+            if(tempHS.size()>1) {
+                headerColors = view.getResources().obtainTypedArray(R.array.header_color);
+
+                labels.add(view.getResources().getString(R.string.spec_tire_size_winter));
+                labels.add(view.getResources().getString(R.string.spec_tire_size_summer));
+
+                //Header
+                TextView tempHeaderTitle = new TextView(view.getContext());
+
+                tempHeaderTitle.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                tempHeaderTitle.setPadding(hMargin, vMargin, hMargin, hMargin);
+                tempHeaderTitle.setTextColor(view.getResources().getColor(R.color.header));
+                tempHeaderTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, headerSize);
+                tempHeaderTitle.setText(R.string.header_tires);
+                tempHeaderTitle.setBackgroundColor(headerColors.getColor(2, 0));
+
+                ((LinearLayout) layout).addView(tempHeaderTitle);
+
+                for (int i = 0; i < cardInfo.size(); i++) {
+                    if (!cardInfo.get(i).isEmpty()) {
+                        TextView tempTextView = new TextView(view.getContext());
+
+                        tempTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                        tempTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fieldSize);
+                        tempTextView.setTextColor(view.getResources().getColor(R.color.secondary_text));
+
+                        tempTextView.setPadding(hcontentSpace, vcontentSpace, hcontentSpace, vcontentSpace);
+                        tempTextView.setGravity(Gravity.START);
+
+                        tempTextView.setText(labels.get(i) + ":     " + cardInfo.get(i));
+
+                        ((LinearLayout) layout).addView(tempTextView);
+
+                    }
+                }
             }
-
         }
+    }
