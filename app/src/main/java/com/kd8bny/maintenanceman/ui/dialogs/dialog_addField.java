b729f73d6f00ee2ff1ec8b156eb0e7b9353a25ab
kd8bny <kd8bny@gmail.com>
2015-03-23 22:46:52 -0400

    init commit

---
+++ app/src/main/java/com/kd8bny/maintenanceman/ui/dialogs/dialog_addField.java
@@ -0,0 +1,87 @@
+package com.kd8bny.maintenanceman.ui.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.kd8bny.maintenanceman.R;
+
+import java.util.ArrayList;
+
+
+public class dialog_addField extends DialogFragment{
+    private static final String TAG = "dlg_add_fld";
+
+    private Spinner spinnerFieldType;
+    private String fieldType;
+    private String fieldName;
+    private String fieldVal;
+
+
+    public dialog_addField(){}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_add_field, null);
+
+        //Spinner
+        spinnerFieldType = (Spinner) view.findViewById(R.id.spinner_field_type);
+        final String [] mfieldTypes = getActivity().getResources().getStringArray(R.array.field_type);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, mfieldTypes);
+        spinnerFieldType.setAdapter(spinnerAdapter);
+
+        final EditText editFieldName = (EditText) view.findViewById(R.id.field_name);
+        final EditText editFieldVal = (EditText) view.findViewById(R.id.field_val);
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity())
+            .setTitle(R.string.title_add_field)
+            .setPositiveButton("OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            // DO SOMETHING
+                            fieldType = spinnerFieldType.getSelectedItem().toString();
+                            fieldName = editFieldName.getText().toString();
+                            fieldVal = editFieldVal.getText().toString();
+                            sendResult(0);
+                        }
+                    }
+            )
+            .setNegativeButton("Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            // DO SOMETHING
+                        }
+                    }
+            );
+
+        alertDialog.setView(view);
+
+        return alertDialog.create();
+    }
+
+    private void sendResult(int REQUEST_CODE) {
+        ArrayList<String> temp = new ArrayList<>();
+        temp.add(fieldName);
+        temp.add(fieldVal);
+        Intent intent = new Intent();
+        intent.putExtra("category", fieldType);
+        intent.putExtra("fieldData", temp);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), REQUEST_CODE, intent);
+    }
+}
