56ba195e3d2bae456730da94fce39476d9e2a674
kd8bny <kd8bny@gmail.com>
2015-05-15 16:41:18 -0500

    Rebuilt event string

--- app/src/main/java/com/kd8bny/maintenanceman/ui/dialogs/dialog_vehicleHistory.java
+++ app/src/main/java/com/kd8bny/maintenanceman/ui/dialogs/dialog_vehicleHistory.java
@@ -1,8 +1,11 @@
 package com.kd8bny.maintenanceman.ui.dialogs;
 
 import android.app.DialogFragment;
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +23,14 @@ public class dialog_vehicleHistory extends DialogFragment{
 
     public ArrayList<String> event;
 
-    public dialog_vehicleHistory(){}
+    public dialog_vehicleHistory(){
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Bundle args = getArguments();
         if (args != null){
             event = (ArrayList) args.getSerializable("event");
@@ -37,8 +43,15 @@ public class dialog_vehicleHistory extends DialogFragment{
         View view = inflater.inflate(R.layout.dialog_vehicle_history, container, false);
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 
-        ((TextView)view.findViewById(R.id.val_spec_date)).setText(event.get(1));
-        ((TextView)view.findViewById(R.id.val_spec_odo)).setText(event.get(2));
+        SpannableStringBuilder dateString = new SpannableStringBuilder("Completed on " + event.get(1));
+        SpannableStringBuilder odoString = new SpannableStringBuilder(" at " + event.get(2) + " mi");
+        StyleSpan styleSpan = new StyleSpan(android.graphics.Typeface.BOLD);
+        StyleSpan styleSpan2 = new StyleSpan(android.graphics.Typeface.BOLD);
+
+        dateString.setSpan(styleSpan, 10, 13, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        odoString.setSpan(styleSpan2, 0, 4, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+        ((TextView) view.findViewById(R.id.val_spec_date_and_odo)).setText(TextUtils.concat(dateString, odoString));
         ((TextView)view.findViewById(R.id.val_spec_event)).setText(event.get(3));
         ((TextView)view.findViewById(R.id.val_spec_price)).setText(event.get(4));
         ((TextView)view.findViewById(R.id.val_spec_comment)).setText(event.get(5));
