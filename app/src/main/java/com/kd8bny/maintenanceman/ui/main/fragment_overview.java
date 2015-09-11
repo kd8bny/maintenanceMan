e5b8298406bfc38409d614777af1b1a793ac44d2
kd8bny <kd8bny@gmail.com>
2015-09-11 14:31:29 -0500

    Add support for FAB and FAM snackbar transistion gist from https://gist.github.com/mmazzarolo/a5d780807fdfa93ee7ed edit for Clans FAB support

--- app/src/main/java/com/kd8bny/maintenanceman/ui/main/fragment_overview.java
+++ app/src/main/java/com/kd8bny/maintenanceman/ui/main/fragment_overview.java
@@ -19,7 +19,6 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import com.kd8bny.maintenanceman.BuildConfig;
 import com.kd8bny.maintenanceman.R;
