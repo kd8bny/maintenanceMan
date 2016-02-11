bd417ec306a02ba6e1e2e78d1b717ea03731192c
kd8bny <kd8bny@gmail.com>
2016-02-11 17:56:12 -0600

    Add option to quick add new spec

--- app/src/main/java/com/kd8bny/maintenanceman/fragments/fragment_info.java
+++ app/src/main/java/com/kd8bny/maintenanceman/fragments/fragment_info.java
@@ -1,34 +1,30 @@
 package com.kd8bny.maintenanceman.fragments;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
 
-import com.github.clans.fab.FloatingActionMenu;
+import com.github.clans.fab.FloatingActionButton;
 import com.kd8bny.maintenanceman.R;
+import com.kd8bny.maintenanceman.activities.VehicleActivity;
 import com.kd8bny.maintenanceman.adapters.adapter_info;
 import com.kd8bny.maintenanceman.classes.Vehicle.Vehicle;
-import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.kd8bny.maintenanceman.classes.data.SaveLoadHelper;
+import com.kd8bny.maintenanceman.dialogs.dialog_addField;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class fragment_info extends Fragment {
     private static final String TAG = "frgmnt_inf";
@@ -51,9 +47,11 @@ public class fragment_info extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         context = getActivity().getApplicationContext();
+
         Bundle bundle = getActivity().getIntent().getBundleExtra("bundle");
+        roster = bundle.getParcelableArrayList("roster");
         vehiclePos = bundle.getInt("vehiclePos", -1);
-        vehicle = bundle.getParcelable("vehicle");
+        vehicle = roster.get(vehiclePos);
     }
 
     @Override
@@ -68,23 +66,70 @@ public class fragment_info extends Fragment {
         cardListAdapter = new adapter_info(vehicle);
         cardList.setAdapter(cardListAdapter);
 
-        return view;
-    }
+        //menu_overview_fab
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                dialog_addField dialog_addField = new dialog_addField();
+                dialog_addField.setTargetFragment(fragment_info.this, 0);
+                dialog_addField.show(fm, "dialog_add_field");
+            }
+        });
 
-    @Override
-    public void onStart(){
-        super.onStart();
+        return view;
     }
 
     @Override
     public void onResume(){
         super.onResume();
+        cardListAdapter = new adapter_info(vehicle);
+        cardList.setAdapter(cardListAdapter);
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "save");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ArrayList<String> result = data.getStringArrayListExtra("fieldData");
+
+        switch (resultCode) {
+            case (1):
+                HashMap<String, String> temp;
+                switch (result.get(0)) {//TODO replace changed key
+                    case "General":
+                        temp = vehicle.getGeneralSpecs();
+                        temp.put(result.get(1), result.get(2));
+                        vehicle.setGeneralSpecs(temp);
+                        break;
+                    case "Engine":
+                        temp = vehicle.getEngineSpecs();
+                        temp.put(result.get(1), result.get(2));
+                        vehicle.setEngineSpecs(temp);
+                        break;
+                    case "Power Train":
+                        temp = vehicle.getPowerTrainSpecs();
+                        temp.put(result.get(1), result.get(2));
+                        vehicle.setPowerTrainSpecs(temp);
+                        break;
+                    case "Other":
+                        temp = vehicle.getOtherSpecs();
+                        temp.put(result.get(1), result.get(2));
+                        vehicle.setOtherSpecs(temp);
+                        break;
+                }
+            break;
+        }
+        roster.set(vehiclePos, vehicle);
+        new SaveLoadHelper(context).save(roster);
+
+        onResume();
     }
 
     @Override
@@ -96,41 +141,13 @@ public class fragment_info extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem menuitem) {
         switch (menuitem.getItemId()) {
-            case R.id.menu_del:
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setCancelable(true);
-                builder.setTitle("Are you sure you would like to delete this vehicle?");
-                builder.setNegativeButton("No", null);
-                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface dialog, int which) {
-                        /*fleetRosterJSONHelper fltjson = new fleetRosterJSONHelper();
-                        fltjson.deleteEntry(getActivity(), refID);
-
-                        vehicleLogDBHelper vhclDBHlpr = new vehicleLogDBHelper(getActivity().getApplicationContext());
-                        vhclDBHlpr.purgeHistory(getActivity().getApplicationContext(), refID);
-
-                        backupRestoreHelper mbackupRestoreHelper = new backupRestoreHelper();
-                        mbackupRestoreHelper.startAction(getActivity().getApplicationContext(), "backup", false);
-
-                        getActivity().finish();*/
-                    }});
-
-                builder.show();
-
-                return true;
-
             case R.id.menu_edit:
                 Bundle bundle = new Bundle();
+                bundle.putInt("caseID", 2);
                 bundle.putInt("vehiclePos", vehiclePos);
-                bundle.putParcelable("vehicle", vehicle);
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                Fragment fragment = new fragment_fleetRoster_edit();
-                fragment.setArguments(bundle);
-                ft.replace(R.id.fragment_container, fragment);
-                ft.addToBackStack("info");
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                ft.commit();
+                bundle.putParcelableArrayList("roster", roster);
+                Log.d(TAG, roster.toString());
+                startActivity(new Intent(getActivity(), VehicleActivity.class).putExtra("bundle", bundle));
 
                 return true;
 
