d9d87001603c850e535d9c3b599058f71666a5ff
kd8bny <kd8bny@gmail.com>
2016-02-11 20:00:44 -0600

    Fix nav menu

--- app/src/main/java/com/kd8bny/maintenanceman/activities/MainActivity.java
+++ app/src/main/java/com/kd8bny/maintenanceman/activities/MainActivity.java
@@ -2,22 +2,11 @@ package com.kd8bny.maintenanceman.activities;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.kd8bny.maintenanceman.R;
-import com.kd8bny.maintenanceman.dialogs.dialog_donate;
-import com.kd8bny.maintenanceman.fragments.fragment_overview;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.kd8bny.maintenanceman.fragments.fragment_main;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,91 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setHomeButtonEnabled(true);
-
-
-        //Drawer
-        final DrawerBuilder drawerBuilder = new DrawerBuilder(this);
-        drawerBuilder.withActionBarDrawerToggle(true)
-                .withActionBarDrawerToggleAnimated(true)
-                .withTranslucentStatusBar(true)
-                .withToolbar(toolbar)
-                .withCloseOnClick(true)
-                .withActionBarDrawerToggleAnimated(true)
-                .withSelectedItem(-1)
-                .withHeader(R.layout.drawer_header)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withName(R.string.title_add_fleet_roster).withIcon(R.drawable.ic_action_add_fleet),
-                        new PrimaryDrawerItem().withName(R.string.title_add_vehicle_event).withIcon(R.drawable.ic_action_add_event),
-                        new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName(R.string.title_settings),
-                        new SecondaryDrawerItem().withName(R.string.title_donate),
-                        new SecondaryDrawerItem().withName(R.string.drawer_view_community));
-
-        final Drawer drawer = drawerBuilder.build();
-        //drawer.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
-        /*drawer.setOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-            @Override
-            public boolean onItemClick(View view, int i, IDrawerItem iDrawerItem) {
-                Intent intent = new Intent(getActivity(), VehicleActivity.class);
-                Bundle bundle = new Bundle();
-                switch (i) {
-                    case 1: //Add Vehicle
-                        bundle.putInt("caseID", 0);
-                        bundle.putParcelableArrayList("roster", roster);
-                        bundle.putInt("vehiclePos", -1);
-                        intent.putExtra("bundle", bundle);
-                        view.getContext().startActivity(intent);
-                        drawer.closeDrawer();
-
-                        return true;
-
-                    case 2: //Add Event
-                        bundle.putInt("caseID", 1);
-                        bundle.putParcelableArrayList("roster", roster);
-                        intent.putExtra("bundle", bundle);
-                        view.getContext().startActivity(intent);
-                        drawer.closeDrawer();
-
-                        return true;
-
-                    case 4: //Settings
-                        Intent settingsIntent = new Intent(view.getContext(), SettingsActivity.class);
-                        view.getContext().startActivity(settingsIntent);
-                        drawer.closeDrawer();
-
-                        return true;
-
-                    case 5: //Donate
-                        FragmentManager fm = getFragmentManager();
-
-                        dialog_donate dialog_donate = new dialog_donate();
-                        dialog_donate.show(fm, "dialog_donate");
-                        drawer.closeDrawer();
-
-                        return true;
-
-                    case 6: //Community
-                        Uri gplus = Uri.parse("https://plus.google.com/u/0/communities/102216501931497148667");
-                        Intent gplusIntent = new Intent(Intent.ACTION_VIEW, gplus);
-                        startActivity(gplusIntent);
-                        drawer.closeDrawer();
-
-                        return true;
-                }
-                return false;
-            }});*/
-
-
-
         FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_main);
 
         if (fragment == null){
-            fragment = new fragment_overview();
+            fragment = new fragment_main();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
--- app/src/main/java/com/kd8bny/maintenanceman/fragments/fragment_main.java
+++ app/src/main/java/com/kd8bny/maintenanceman/fragments/fragment_main.java
@@ -5,10 +5,13 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,22 +19,30 @@ import android.view.ViewGroup;
 
 import com.kd8bny.maintenanceman.BuildConfig;
 import com.kd8bny.maintenanceman.R;
+import com.kd8bny.maintenanceman.activities.SettingsActivity;
 import com.kd8bny.maintenanceman.activities.VehicleActivity;
 import com.kd8bny.maintenanceman.activities.ViewPagerActivity;
 import com.kd8bny.maintenanceman.adapters.adapter_overview;
 import com.kd8bny.maintenanceman.classes.Vehicle.Vehicle;
 import com.kd8bny.maintenanceman.classes.data.SaveLoadHelper;
 import com.kd8bny.maintenanceman.classes.data.BackupRestoreHelper;
+import com.kd8bny.maintenanceman.dialogs.dialog_donate;
 import com.kd8bny.maintenanceman.interfaces.UpdateUI;
 import com.kd8bny.maintenanceman.listeners.RecyclerViewOnItemClickListener;
 import com.kd8bny.maintenanceman.dialogs.dialog_whatsNew;
 import com.kd8bny.maintenanceman.activities.IntroActivity;
 
 import com.github.clans.fab.FloatingActionMenu;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.util.ArrayList;
 
-public class fragment_overview extends Fragment implements UpdateUI{
+public class fragment_main extends Fragment implements UpdateUI{
     private static final String TAG = "frg_ovrvw";
 
     private final String SHARED_PREF = "com.kd8bny.maintenanceman_preferences";
@@ -45,7 +56,7 @@ public class fragment_overview extends Fragment implements UpdateUI{
     private ArrayList<Vehicle> roster;
     private String mUnit;
 
-    public fragment_overview() {}
+    public fragment_main() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -64,6 +75,84 @@ public class fragment_overview extends Fragment implements UpdateUI{
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Toolbar
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.tool_bar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+
+        //Drawer
+        final DrawerBuilder drawerBuilder = new DrawerBuilder(getActivity());
+        drawerBuilder.withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withTranslucentStatusBar(true)
+                .withToolbar(toolbar)
+                .withCloseOnClick(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withSelectedItem(-1)
+                .withHeader(R.layout.drawer_header)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.title_add_fleet_roster).withIcon(R.drawable.ic_action_add_fleet),
+                        new PrimaryDrawerItem().withName(R.string.title_add_vehicle_event).withIcon(R.drawable.ic_action_add_event),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName(R.string.title_settings),
+                        new SecondaryDrawerItem().withName(R.string.title_donate),
+                        new SecondaryDrawerItem().withName(R.string.drawer_view_community));
+
+        final Drawer drawer = drawerBuilder.build();
+        drawer.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+        drawer.setOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int i, IDrawerItem iDrawerItem) {
+                Intent intent = new Intent(getActivity(), VehicleActivity.class);
+                Bundle bundle = new Bundle();
+                switch (i) {
+                    case 1: //Add Vehicle
+                        bundle.putInt("caseID", 0);
+                        bundle.putParcelableArrayList("roster", roster);
+                        bundle.putInt("vehiclePos", -1);
+                        intent.putExtra("bundle", bundle);
+                        view.getContext().startActivity(intent);
+                        drawer.closeDrawer();
+
+                        return true;
+
+                    case 2: //Add Event
+                        bundle.putInt("caseID", 1);
+                        bundle.putParcelableArrayList("roster", roster);
+                        intent.putExtra("bundle", bundle);
+                        view.getContext().startActivity(intent);
+                        drawer.closeDrawer();
+
+                        return true;
+
+                    case 4: //Settings
+                        Intent settingsIntent = new Intent(view.getContext(), SettingsActivity.class);
+                        view.getContext().startActivity(settingsIntent);
+                        drawer.closeDrawer();
+
+                        return true;
+
+                    case 5: //Donate
+                        FragmentManager fm = getFragmentManager();
+
+                        dialog_donate dialog_donate = new dialog_donate();
+                        dialog_donate.show(fm, "dialog_donate");
+                        drawer.closeDrawer();
+
+                        return true;
+
+                    case 6: //Community
+                        Uri gplus = Uri.parse("https://plus.google.com/u/0/communities/102216501931497148667");
+                        Intent gplusIntent = new Intent(Intent.ACTION_VIEW, gplus);
+                        startActivity(gplusIntent);
+                        drawer.closeDrawer();
+
+                        return true;
+                }
+                return false;
+            }});
 
         //cards
         mUnit = sharedPreferences.getString("prefUnitDist", "");
