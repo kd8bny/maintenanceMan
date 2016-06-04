ad2f5826e3c55fa0498a56aeb7425808c4833c34
kd8bny <kd8bny@gmail.com>
2016-06-04 13:50:35 -0500

    Remove dropbox advanced options

--- app/src/main/java/com/kd8bny/maintenanceman/fragments/fragment_settings_dbx.java
+++ app/src/main/java/com/kd8bny/maintenanceman/fragments/fragment_settings_dbx.java
@@ -5,17 +5,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.support.design.widget.Snackbar;
 import android.util.Log;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.session.AppKeyPair;
 import com.kd8bny.maintenanceman.R;
-import com.kd8bny.maintenanceman.classes.data.BackupRestoreHelper;
-import com.kd8bny.maintenanceman.classes.data.SaveLoadHelper;
-
-import java.io.File;
 
 
 public class fragment_settings_dbx extends PreferenceFragment{
@@ -24,31 +19,23 @@ public class fragment_settings_dbx extends PreferenceFragment{
     private DropboxAPI<AndroidAuthSession> mDBApi;
 
     private static final String SHARED_PREF = "com.kd8bny.maintenanceman_preferences";
-    private static final String FILE_NAME = "fleetRoster.json";
-    private Context context;
+    private Context mContext;
 
-    public fragment_settings_dbx() {
-
-    }
+    public fragment_settings_dbx() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences_dbx);
 
-        context = getActivity().getApplicationContext();
+        mContext = getActivity().getApplicationContext();
         final Preference setupButton = findPreference(getString(R.string.pref_title_cloud_init));
-        final Preference backupButton = findPreference(getString(R.string.pref_title_cloud_backup));
-        final Preference restoreButton = findPreference(getString(R.string.pref_title_cloud_restore));
-        final String APP_KEY = context.getResources().getString(R.string.dropboxKey);
-        final String APP_SECRET = context.getResources().getString(R.string.dropboxSecret);
+        final String APP_KEY = mContext.getResources().getString(R.string.dropboxKey);
+        final String APP_SECRET = mContext.getResources().getString(R.string.dropboxSecret);
         AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
         AndroidAuthSession session = new AndroidAuthSession(appKeys);
 
         mDBApi = new DropboxAPI<>(session);
-
-        if (getActivity().getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE).getString(getString(R.string.pref_key_dropbox), null) != null) {
-        }
 
         setupButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
@@ -58,27 +45,6 @@ public class fragment_settings_dbx extends PreferenceFragment{
                 return true;
             }
         });
-        /*backupButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                BackupRestoreHelper mbackupRestoreHelper = new BackupRestoreHelper();
-                mbackupRestoreHelper.startAction(getActivity().getApplicationContext(), "backup", true);
-
-                Snackbar.make(getActivity().findViewById(R.id.fragmentContainer_settings), getString(R.string.pref_toast_dropbox_backup), Snackbar.LENGTH_SHORT).show();
-
-                return true;
-            }
-        });
-        restoreButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                BackupRestoreHelper mbackupRestoreHelper = new BackupRestoreHelper();
-                mbackupRestoreHelper.startAction(getActivity().getApplicationContext(), "restore", true);
-
-                Snackbar.make(getActivity().findViewById(R.id.fragmentContainer_settings), getString(R.string.pref_toast_dropbox_restore), Snackbar.LENGTH_SHORT).show();
-                return true;
-            }
-        });*/
     }
 
     @Override
@@ -94,23 +60,6 @@ public class fragment_settings_dbx extends PreferenceFragment{
                 SharedPreferences.Editor editor= sharedPreferences.edit();
                 editor.putString(getString(R.string.pref_key_dropbox), accessToken);
                 editor.apply();
-
-
-                /*File file = new File(context.getFilesDir() + "/" + FILE_NAME);
-                if ((new SaveLoadHelper(getActivity().getApplicationContext()).load()).isEmpty()){
-                    BackupRestoreHelper mbackupRestoreHelper = new BackupRestoreHelper();
-                    mbackupRestoreHelper.startAction(getActivity().getApplicationContext(), "restore", true);
-                }else{
-                    BackupRestoreHelper mbackupRestoreHelper = new BackupRestoreHelper();
-                    mbackupRestoreHelper.startAction(getActivity().getApplicationContext(), "backup", true);
-                }*/
-
-                /*PackageManager packageManager = context.getPackageManager();
-                Intent intent = packageManager.getLaunchIntentForPackage(context.getPackageName());
-                ComponentName componentName = intent.getComponent();
-                Intent mainIntent = IntentCompat.makeRestartActivityTask(componentName);
-                context.startActivity(mainIntent);
-                System.exit(0);*/
 
             } catch (IllegalStateException e) {
                 Log.i("DbAuthLog", "DROPBOX: Error authenticating", e);
