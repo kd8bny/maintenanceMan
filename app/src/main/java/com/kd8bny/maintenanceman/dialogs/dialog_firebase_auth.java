1768fd5d54159d477c44ddc87095b0f9981bf526
kd8bny <kd8bny@gmail.com>
2017-12-03 13:37:59 -0600

    Dismiss dialog on successful log in

--- app/src/main/java/com/kd8bny/maintenanceman/dialogs/dialog_firebase_auth.java
+++ app/src/main/java/com/kd8bny/maintenanceman/dialogs/dialog_firebase_auth.java
@@ -91,7 +91,7 @@ public class dialog_firebase_auth extends DialogFragment {
                                     if (task.isSuccessful()) {
                                         // Sign in success, update UI with the signed-in user's information
                                         Log.d(TAG, "signInWithEmail:success");
-                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        alertDialog.dismiss();
 
                                     } else {
 
@@ -112,7 +112,7 @@ public class dialog_firebase_auth extends DialogFragment {
                                     }
                                 }
                             });
-            }});
+                }});
 
             buttonCreate.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -127,7 +127,7 @@ public class dialog_firebase_auth extends DialogFragment {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    alertDialog.dismiss();
 
                                 } else {
                                     // If sign in fails, display a message to the user.
@@ -139,7 +139,7 @@ public class dialog_firebase_auth extends DialogFragment {
 
                                 // ...
                             }
-                    });
+                        });
                 }});
         }
     }
